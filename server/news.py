8ed68781025c32b78e81ec03861d46dac39a694f
Ray <sarge0087@gmail.com>
2025-01-25 21:20:08 -0500

    news update

--- server/news.py
+++ server/news.py
@@ -1,5 +1,6 @@
 import feedparser as fp
 import json
+from datetime import datetime
 import time
 from json import JSONDecodeError
 from app import app
@@ -9,22 +10,40 @@ articles = []
 
 while True:
     start = time.time()
-    print("Fetching...")
+    curr_time = time.strftime("%H:%M:%S", time.localtime())
+    print(f"({curr_time}) Fetching GlobeNewsWire...")
     url = "https://www.globenewswire.com/RssFeed/orgclass/1/feedTitle/GlobeNewswire%20-%20News%20about%20Public%20Companies"
     feed = fp.parse(url)
     
     with app.app_context():
     
-    #     # db.session.query(Article).delete()
-    #     # db.session.commit()
+        # db.session.query(Article).delete()
+        # db.session.commit()
 
         for entry in feed.entries:
+            try:
+                year = entry.published_parsed[0]
+                month = entry.published_parsed[1]
+                day = entry.published_parsed[2]
+                hour = entry.published_parsed[3]
+                minute = entry.published_parsed[4]
+                second = entry.published_parsed[5]
+                published = datetime(year, month, day, hour, minute, second)
+                str_time = f"({hour:02d}:{minute:02d}:{second:02d})"
+            except IndexError:
+                published = datetime(0, 0, 0, 0, 0, 0)
+                str_time = "Time error"
+                print("Index out of range")
+                pass
+
             article = Article(
                         title = entry.title,
                         source = entry.publisher,
                         source_url = entry.id,
-                        companies = []
+                        companies = [],
+                        date_time = published
                     )
+            
             for tag in entry.tags:
                 if any(exchange.lower() in tag.term.lower() for exchange in ('Nasdaq', 'Nyse')):
                     try:
@@ -39,7 +58,7 @@ while True:
                         article_exists = True
                         break
                 if not article_exists:  # Only add if article doesn't exist
-                    print(article.companies, article.title)
+                    print("Published:", str_time, article.companies, article.title)
                     articles.append(article)
         
         if len(articles) > 0:
@@ -49,8 +68,8 @@ while True:
                     
     end = time.time()
     # print (end - start)
-    # test_file = open("testfile.json", "w")
-    # dump = json.dump(feed, test_file, indent = 4)
+    test_file = open("testfile.json", "w")
+    dump = json.dump(feed, test_file, indent = 4)
     time.sleep(15)
 
 
