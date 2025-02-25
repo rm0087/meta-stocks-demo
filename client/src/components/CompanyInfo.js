6c6fe887e77cdf6abd9e9756af6352abd666db80
rm0087 <sarge0087@gmail.com>
2025-02-25 00:39:12 -0500

    filing categories

--- client/src/components/CompanyInfo.js
+++ client/src/components/CompanyInfo.js
@@ -1,16 +1,50 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 export default function CompanyInfo({company, filings}) {
-
+    const [latestFilings, setLatestFilings] = useState([])
+    const [finFilings, setFinFilings] = useState([])
+    const [insiderFilings, setInsiderFilings] = useState([]);
+    const [institutionalFilings, setInstitutionalFilings] = useState([]);
     
-    const filingsList = filings && filings.latest.map((filing,index)=> {
-        return (
-        <tr key={index}><td>{filing.date} - <a target="_blank" href={filing.url}>Form {filing.form}</a></td></tr>)})
+
+    useEffect(()=>{
         
+            const latest = filings?.latest?.map((filing,index)=> {
+                return (
+                    <tr key={index}><td>{filing.date} - <a target="_blank" href={filing.url}>{filing.form}</a></td></tr>
+                )
+            })
+            setLatestFilings(latest)
+
+            const fins = filings?.fin?.map((filing,index)=> {
+                return (
+                    <tr key={index}><td>{filing.date} - <a target="_blank" href={filing.url}>{filing.form}</a></td></tr>
+                )
+            })
+            setFinFilings(fins)
+
+            const insiders = filings?.insiders?.map((filing,index)=> {
+                return (
+                    <tr key={index}><td>{filing.date} - <a target="_blank" href={filing.url}>{filing.form}</a></td></tr>
+                )
+            })
+            setInsiderFilings(insiders)
+
+            const institutionals = filings?.institutions?.map((filing,index)=> {
+                return (
+                    <tr key={index}><td>{filing.date} - <a target="_blank" href={filing.url}>{filing.form}</a></td></tr>
+                )
+            })
+            setInstitutionalFilings(institutionals)
+        
+    },[filings])
+    
+    
+    
     return (
         <div className="w-full justify-center mt-5">
             <div className="w-[95%] flex flex-row justify-center">
-            <div className="w-[47%] font-mono tracking-tight font-mono tracking-tight text-xs border rounded text-white">
+            <div className="w-[25%] font-mono tracking-tight font-mono tracking-tight text-xs border rounded text-white">
             <div className="px-5 py-2">
                 <h2 className="text-lg font-bold">🏢 Company Info.</h2>
                 <table>
@@ -25,15 +59,36 @@ export default function CompanyInfo({company, filings}) {
                 </table>
             </div>
             </div>
-            <div className="w-[47%] font-mono tracking-tight font-mono tracking-tight text-xs border rounded text-white">
+            <div className="w-[75%] font-mono tracking-tight font-mono tracking-tight text-xs border rounded text-white">
             <div className="px-5 py-2">
+                
                 <h2 className="text-lg font-bold">🏢 Filings</h2>
-                <table>
-                    <tbody>
-                        {filingsList}
-                    </tbody>
-                </table>
-
+                <div className="md:grid grid-cols-6 gap-4 place-items-center w-full h-full text-gray-50 font-mono text-xs">
+                    <table className="">
+                        <tbody>
+                            <h2 className="font-bold text-sm">Latest</h2>
+                            {latestFilings}
+                        </tbody>
+                    </table>
+                    <table>
+                        <tbody>
+                            <h2 className="font-bold text-sm">Financial Reports</h2>
+                            {finFilings}
+                        </tbody>
+                    </table>
+                    <table>
+                        <tbody>
+                            <h2 className="font-bold text-sm">Insiders</h2>
+                            {insiderFilings}
+                        </tbody>
+                    </table>
+                    <table>
+                        <tbody>
+                            <h2 className="font-bold text-sm">Institutions</h2>
+                            {institutionalFilings}
+                        </tbody>
+                    </table>
+                </div>               
 
 
             </div>
